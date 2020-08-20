f43ee2727bc423ef2d3df4577ae25094194a0423
Stony Wu <stonywu@uw.edu>
2020-08-20 19:32:26 -0400

    move the script in html, change the name of views

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-// Fetch the data and store it in a global variable gameList[]
+// Switch between two views
+function switchview(checkbox) {
+    var all = document.getElementById("listView");
+    var img = document.getElementById("cardView");
+    if (checkbox.checked) {
+        img.style.display = "block"
+        all.style.display = "none"
+    } else {
+        all.style.display = "block"
+        img.style.display = "none"
+    }
+}
+
+// The array contains the basic game data we will use.
 let gameList = [];
 
-function getGameListByPage(pageNum) {
+// Fetch the data and store it in a global variable gameList[]
+function getGameListByPage(pageNum, pageCounts) {
     let fetchResult = fetch("https://api.rawg.io/api/games?page=" + pageNum)
     .then(function(response) {
         let dataPromise = response.json();
@@ -9,7 +23,11 @@ function getGameListByPage(pageNum) {
     })
     .then(function(data) {
         alterGameList(data);
-        console.log(gameList);
+        if (gameList.length >= (pageCounts * 20)){
+            console.log(gameList);
+            //renderListView(gameList);
+            //renderCardView(gameList);
+        }
         return data;
     })
     .catch(err => {
@@ -18,17 +36,45 @@ function getGameListByPage(pageNum) {
     return fetchResult;
 }
 
+// Add the game to the global gameList[]
 function alterGameList(data) {
     for (let each of data.results) {
         gameList.push(each);
     }
 }
 
+// Get game list base on the pageCounts, each page has 20 games
 function getGameList(pageCounts) {
     for (let i = 1; i <= pageCounts; i++) {
-        getGameListByPage(i);
+        getGameListByPage(i, pageCounts);
     }
 }
 
+// Get initial gameList[], here we want 100 games which is 5 pages
 getGameList(5);
+
+function renderListView(list) {
+    for (let each of list) {
+        renderSingleListView(each);
+    }
+}
+
+function renderCardView(list) {
+    for (let each of list) {
+        renderSingleCardView(each);
+    }
+}
+
+function renderSingleListView(game) {
+
+}
+
+function renderSingleCardView(game) {
+
+}
+
+
+
+
+
 
