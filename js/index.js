5cb9f2b9e4ad9376f451d601ac7ea7438005a201
Stony Wu <stonywu@uw.edu>
2020-08-21 17:46:31 -0400

    fetch and render platforms/genres

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+// The array contains the basic game data we will use.
+let gameList = [];
+let genreList = [];
+let platformList = [];
+
+// Get initial gameList[], here we want 100 games which is 5 pages
+getGameList(5);
+// initialize genre list 
+getGenreList();
+// initialize platform list
+getPlatformList();
+
 // Switch between two views
 function switchview(checkbox) {
     let listViewElem = document.getElementById("listView");
@@ -12,10 +24,6 @@ function switchview(checkbox) {
         listViewElem.style.display = "block"
     }
 }
-
-
-// The array contains the basic game data we will use.
-let gameList = [];
 
 // Fetch the data and store it in a global variable gameList[]
 function getGameListByPage(pageNum, pageCounts) {
@@ -45,6 +53,21 @@ function alterGameList(data) {
     }
 }
 
+// Add the genre to the global genreList[]
+function alterGenreList(data) {
+    for (let each of data.results) {
+        genreList.push(each);
+    }
+}
+
+// Add the platform to tge global platformList[]
+// Only take first 22 platform
+function alterPlatformList(data) {
+    for (let i = 0; i < 22; i++) {
+        platformList.push(data.results[i]);
+    }
+}
+
 // Get game list base on the pageCounts, each page has 20 games
 function getGameList(pageCounts) {
     for (let i = 1; i <= pageCounts; i++) {
@@ -52,21 +75,59 @@ function getGameList(pageCounts) {
     }
 }
 
-// Get initial gameList[], here we want 100 games which is 5 pages
-getGameList(5);
+// Get and render the genre list
+function getGenreList(){
+    let fetchResult = fetch("https://api.rawg.io/api/genres")
+    .then(function(response) {
+        let dataPromise = response.json();
+        return dataPromise;
+    })
+    .then(function(data) {
+        alterGenreList(data);
+        renderGenreSelector();
+        return data;
+    })
+    .catch(err => {
+	console.log(err);
+    });
+    return fetchResult;
+}
 
+// Get and render the platform list
+function getPlatformList(){
+    let fetchResult = fetch("https://api.rawg.io/api/platforms")
+    .then(function(response) {
+        let dataPromise = response.json();
+        return dataPromise;
+    })
+    .then(function(data) {
+        alterPlatformList(data);
+        renderPlatformSelector();
+        return data;
+    })
+    .catch(err => {
+	console.log(err);
+    });
+    return fetchResult;
+}
+
+// Render the list view base on the array passed in 
 function renderListView(list) {
+    document.querySelector("#listView").innerHTML = "";
     for (let each of list) {
         renderSingleListView(each);
     }
 }
 
+// Render the card view base on the array passed in 
 function renderCardView(list) {
+    document.querySelector("#cardView").innerHTML = "";
     for (let each of list) {
         renderSingleCardView(each);
     }
 }
 
+// Render a single list view block
 function renderSingleListView(game) {
     let cardElem = document.createElement("div");
     cardElem.classList.add("card");
@@ -104,31 +165,7 @@ function renderSingleListView(game) {
     listViewElem.appendChild(cardElem);
 }
 
-function createGenreString(game){
-    let genreList = [];
-    for (let each of game.genres) {
-        genreList.push(each.name);
-    }
-    return arrayToString(genreList);
-}
-
-function createPlatformString(game){
-    let platformList = [];
-    for (let each of game.platforms) {
-        platformList.push(each.platform.name);
-    }
-    return arrayToString(platformList);
-}
-
-function arrayToString(array) {
-    resultString = "";
-    for (let each of array) {
-        resultString = resultString + each + " / ";
-    }
-    resultString = resultString.substring(0, resultString.length - 3);
-    return resultString;
-}
-
+// Render a single card view block
 function renderSingleCardView(game) {
     var cardhtml = 
     (`<div class="col-md-6">
@@ -143,6 +180,66 @@ function renderSingleCardView(game) {
     $('#cardrow').append(cardhtml);
 }
 
+// Return the string of genres
+function createGenreString(game){
+    let genreList = [];
+    for (let each of game.genres) {
+        genreList.push(each.name);
+    }
+    return arrayToString(genreList);
+}
+
+// Return the string of platforms
+function createPlatformString(game){
+    let platformList = [];
+    for (let each of game.platforms) {
+        platformList.push(each.platform.name);
+    }
+    return arrayToString(platformList);
+}
+
+// Convert the array to string
+function arrayToString(array) {
+    resultString = "";
+    for (let each of array) {
+        resultString = resultString + each + " / ";
+    }
+    resultString = resultString.substring(0, resultString.length - 3);
+    return resultString;
+}
+
+// Render genre selector
+function renderGenreSelector() {
+    let genreSelectorElem = document.querySelector("#genre");
+    genreSelectorElem = appendFirstOption(genreSelectorElem);
+    for (let each of genreList) {
+        let optionElem = document.createElement("option");
+        optionElem.value = each.slug;
+        optionElem.textContent = each.name;
+        genreSelectorElem.appendChild(optionElem);
+    }
+}
+
+// Render platform selector
+function renderPlatformSelector() {
+    let platformSelectorElem = document.querySelector("#platform");
+    platformSelectorElem = appendFirstOption(platformSelectorElem);
+    for (let each of platformList) {
+        let optionElem = document.createElement("option");
+        optionElem.value = each.slug;
+        optionElem.textContent = each.name;
+        platformSelectorElem.appendChild(optionElem);
+    }
+}
+
+// Append the first child of selector which is "All"
+function appendFirstOption(parent) {
+    let firstOptionElem = document.createElement("option");
+    firstOptionElem.value = "all";
+    firstOptionElem.textContent = "All";
+    parent.appendChild(firstOptionElem);
+    return parent;
+}
 
 
 
